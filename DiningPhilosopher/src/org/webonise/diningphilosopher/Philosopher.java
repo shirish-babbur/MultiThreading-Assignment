ab21916bf365f5a927e6691f500e94cba4f830fb
shirishb-webonise <bbshirish@gmail.com>
2016-09-21 12:54:32 +0530

    made constants and used access specifiers as suggested.

--- DiningPhilosopher/src/org/webonise/diningphilosopher/Philosopher.java
+++ DiningPhilosopher/src/org/webonise/diningphilosopher/Philosopher.java
@@ -6,13 +6,15 @@ class Philosopher implements Runnable {
     // The philosopher's unique id
     private int id;
     // The chopsticks this philosopher may use
-    private Lock leftChopstick;
-    private Lock rightChopstick;
-    public Philosopher (int id, Lock leftChopstick, Lock rightChopstick) {
+    private final Lock leftChopstick;
+    private final Lock rightChopstick;
+
+    public Philosopher(int id, Lock leftChopstick, Lock rightChopstick) {
         this.id = id;
         this.leftChopstick = leftChopstick;
         this.rightChopstick = rightChopstick;
     }
+
     // Repeatedly think, pick up chopsticks, eat and put down chopsticks
     public void run() {
         try {
@@ -23,32 +25,37 @@ class Philosopher implements Runnable {
                 eat();
                 putDownChopsticks();
             }
-        } catch (Exception e) {
+        } catch (InterruptedException e) {
             System.out.println("Philosopher " + id + " was interrupted.\n");
         }
     }
+
     private void think() throws InterruptedException {
         System.out.println("Philosopher " + id + " is thinking.\n");
         System.out.flush();
-        Thread.sleep (3000);
+        Thread.sleep(2000);
     }
+
     // Locks the left chopstick to signify that this philosopher is holding it
-    private void pickUpLeftChopstick() throws InterruptedException {
+    private void pickUpLeftChopstick() {
         leftChopstick.lock();
         System.out.println("Philosopher " + id + " is holding left chopstick.\n");
         System.out.flush();
     }
+
     // Locks the right chopstick to signify that this philosopher is holding it
-    private void pickUpRightChopstick() throws InterruptedException {
+    private void pickUpRightChopstick() {
         rightChopstick.lock();
         System.out.println("Philosopher " + id + " is holding right chopstick.\n");
         System.out.flush();
     }
+
     private void eat() throws InterruptedException {
         System.out.println("Philosopher " + id + " is eating.\n");
         System.out.flush();
-        Thread.sleep (3000);
+        Thread.sleep(2000);
     }
+
     // Releases the locks on both chopsticks to model putting them down so the
     // other philosophers can use them.
     private void putDownChopsticks() {
